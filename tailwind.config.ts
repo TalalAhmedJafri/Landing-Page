688f68b9a235653d8512262f3e79d3f8ebb932b4
Talal Ahmed Jafri <160124057+TalalAhmedJafri@users.noreply.github.com>
2024-10-23 11:16:11 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,6 @@
 import { url } from "inspector";
 import type { Config } from "tailwindcss";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faB } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const config: Config = {
   content: [
@@ -16,10 +14,6 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       }, 
-      backgroundImage: {
-        Image: "url('https://img.freepik.com/free-vector/gradient-blur-pink-blue-abstract-background_53876-117324.jpg?ga=GA1.1.1305217353.1703097734&semt=ais_hybrid')",
-        Cover: "url('https://img.freepik.com/free-photo/luxury-plain-green-gradient-abstract-studio-background-empty-room-with-space-your-text-picture_1258-88064.jpg?ga=GA1.1.1305217353.1703097734&semt=ais_hybrid')",
-      },
   },
   plugins: [],
 }
